8829e26db181e843e17b8f4fd7581dd784c5caf5
tebellomaila <tebellomakhura@outlook.com>
2026-03-09 22:19:52 +0200

    Add AdaLovelace class for printing 'Ada Lovelace'

---
+++ part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -0,0 +1,7 @@
+// A program to display "Ada Lovelace" on standard output.
+public class AdaLovelace {
+
+    public static void main(String[] args) {
+        System.out.println("Ada Lovelace");
+    }
+}   // end of the class AdaLovelace
