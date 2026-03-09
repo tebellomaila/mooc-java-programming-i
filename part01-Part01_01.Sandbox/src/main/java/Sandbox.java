03c39a40971d9ec587dfdc92a4476b821f2ded74
tebellomaila <tebellomakhura@outlook.com>
2026-03-09 22:00:57 +0200

    Add Javadoc comment documenting Sandbox class purpose

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -1,7 +1,6 @@
-
+// A program to display the message "Welcome to programming!" on standard output.
 public class Sandbox {
-
     public static void main(String[] args) {
-        System.out.println("Welcome to programming!");
+        System.out.println("Welcome to mooc java programming!");
     }
-}
+}   // end of the class Sandbox
